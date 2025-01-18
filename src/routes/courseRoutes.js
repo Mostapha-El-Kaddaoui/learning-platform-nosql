eea3bd68035384e73ff4179761b1a7920691df04
Mostapha-El-Kaddaoui <73528791+Mostapha-El-Kaddaoui@users.noreply.github.com>
2025-01-18 09:50:49 -0500

    Update courseRoutes.js

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -1,7 +1,3 @@
-// Question: Pourquoi séparer les routes dans différents fichiers ?
-// Réponse : Pour améliorer l'organisation, la lisibilité, et la maintenance du code en regroupant les routes par fonctionnalité.
-// Question : Comment organiser les routes de manière cohérente ?
-// Réponse: En les regroupant par domaine dans des fichiers dédiés au sein d'un dossier spécifique, comme /routes.
 const express = require("express");
 const router = express.Router();
 const courseController = require("../controllers/courseController");
